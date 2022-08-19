1148fec890832bca86012aa58679579e9cfbc6b6
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2022-08-19 16:04:40 -0300

    Primeiro commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SafeAreaView, StatusBar, Text, useColorScheme } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const HomePage = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text>Hello World From Home!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomePage;
